bdb68c15c6c3b2078ee411fce39108ce15810e70
bahauddin fahad <bahauddin.fahad819@gmail.com>
2024-06-15 21:24:54 +0600

    updated the readme file

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,17 @@ import cors from 'cors';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
 import router from './app/routes';
-import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 // const port = 3000;
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(cors({ origin: ['http://localhost:5173'] }));
 
 app.use('/api', router);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
+  res.send('Welcome to Bike Rental Service');
 });
 
 app.use(globalErrorHandler);
